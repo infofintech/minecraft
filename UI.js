f08fdd94a7db2fcafcd0d6c9cb89335bffdb1e0f
Vadim Ponomarev <jesuscaesar@outlook.com>
2026-08-19 22:29:38 +1000

    New version v1.2.1

---
+++ UI.js
@@ -0,0 +1,22 @@
+
+// pages
+export * from "./PreloadPage.js";
+export * from "./WelcomePage.js";
+export * from "./SelectWorldPage.js";
+export * from "./HowToPlayPage.js";
+export * from "./SettingPage.js";
+export * from "./LoadTerrainPage.js";
+export * from "./PausePage.js";
+export * from "./PlayPage.js";
+export * from "./CreateNewWorldPage.js";
+
+// mc components
+export * from "./MCButton.js";
+export * from "./MCHotbar.js";
+export * from "./MCCrosshairs.js";
+export * from "./MCFullScreenBtn.js";
+export * from "./MCInventory.js";
+export * from "./MCMoveBtns.js";
+export * from "./MCSlider.js";
+export * from "./MCSwitch.js";
+export * from "./MCInput.js";
